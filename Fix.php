d1812cf0f83becccdd908f4457297f626a6e76ac
Sohel Ahammad <sohel@example.com>
2026-08-20 15:45:32 +0300

    clean: keep only extension source code, landing page and VSIX download hub

--- Fix.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-http_response_code(403);
-exit('Script disabled for security reasons.');
--- test_paths.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-http_response_code(404);
-exit('Not found.');
